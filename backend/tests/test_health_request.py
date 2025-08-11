cbe7a5f1ad4a133913044b5e8f6bdb7e7ff71859
GitHub Action <actions@github.com>
2025-08-11 18:36:35 +0000

    Release frontend nachet-frontend-v0.9.27

--- backend/tests/test_health_request.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from app import app
-
-@pytest.mark.asyncio
-async def test_health():
-    test = app.test_client()
-    response = await test.get('/health')
-    assert response.status_code == 200
