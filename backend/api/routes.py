c6f881897c329caaa465970c4136ff086b3441b2
Joseffus Santos <joseffus.santos@inspection.gc.ca>
2025-09-02 23:40:17 +0000

    remove

--- backend/api/routes.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter, status
-
-router = APIRouter()
-
-# no authentication needed
-@router.get('/health', status_code=status.HTTP_200_OK, name="Get Health Status [NO AUTH REQUIRED]")
-async def get_health_status():
-    return 'OK'
