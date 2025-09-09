338fd9f30e4d7600498463d715f997831383a04a
Joseffus Santos <joseffus.santos@inspection.gc.ca>
2025-09-09 04:49:26 +0000

    settings

--- backend/app/api/config.py
+++ backend/app/api/config.py
@@ -1,80 +1,67 @@
-from starlette.config import Config
+# from starlette.config import Config
 
 
 from contextlib import asynccontextmanager
 from http import HTTPStatus
 
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 from fastapi import APIRouter, FastAPI, Request
 
 # from fastapi.logger import logger
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 
-# from opentelemetry import trace
-# from opentelemetry._logs import set_logger_provider
-# from opentelemetry.exporter.otlp.proto.grpc._log_exporter import OTLPLogExporter
-# from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
-# from opentelemetry.sdk._logs import LoggerProvider, LoggingHandler
-# from opentelemetry.sdk._logs.export import BatchLogRecordProcessor
-# from opentelemetry.sdk.resources import Resource
-# from opentelemetry.sdk.trace import TracerProvider
-# from opentelemetry.sdk.trace.export import BatchSpanProcessor
-from minio import Minio
-from pipeline import GPT, OCR
 from psycopg.conninfo import make_conninfo
 from psycopg_pool import ConnectionPool
-from pydantic import Field, computed_field
+from pydantic import computed_field #, Field
 from pydantic_settings import BaseSettings
 from sqlmodel import StaticPool, create_engine
 
 from app.exceptions import log_error
-from app.models.bucket_name import MinioBucketName
-from app.services.file_storage import FertiscanStorage, MinIOStorageManager
+# from app.models.bucket_name import MinioBucketName
+# from app.services.file_storage import FertiscanStorage, MinIOStorageManager
 
-load_dotenv(".env.secrets")
-load_dotenv(".env.config")
-
-
-config = Config(".env")
-
-API_PREFIX: str = "/api"
-VERSION: str = "0.1.0"
-PROJECT_NAME: str = "FastAPI with AAD Authentication"
-DEBUG: bool = config("DEBUG", cast=bool, default=False)
-
-# Authentication
-API_CLIENT_ID: str = config("API_CLIENT_ID", default="")
-API_CLIENT_SECRET: str = config("API_CLIENT_SECRET", default="")
-SWAGGER_UI_CLIENT_ID: str = config("SWAGGER_UI_CLIENT_ID", default="")
-AAD_TENANT_ID: str = config("AAD_TENANT_ID", default="")
-
-AAD_INSTANCE: str = config("AAD_INSTANCE", default="https://login.microsoftonline.com")
-API_AUDIENCE: str = config("API_AUDIENCE", default=f"api://{API_CLIENT_ID}")
 
 class Settings(BaseSettings):
-    api_endpoint: str = Field(alias="azure_api_endpoint")
-    api_key: str = Field(alias="azure_api_key")
-    base_path: str = Field("", alias="api_base_path")
+    # api_endpoint: str = Field(alias="azure_api_endpoint")
+    # api_key: str = Field(alias="azure_api_key")
+    # base_path: str = Field("", alias="api_base_path")
+    # openai_api_deployment: str = Field(alias="azure_openai_deployment")
+    # openai_api_endpoint: str = Field(alias="azure_openai_endpoint")
+    # openai_api_key: str = Field(alias="azure_openai_key")
+    # phoenix_endpoint: str | None = None
+    # upload_folder: str = "uploads"
+    # otel_exporter_otlp_endpoint: str = Field(alias="otel_exporter_otlp_endpoint")
+
+    # auth settings
+    auth_enabled: bool = True
+    auth_audience: str | None = None
+    auth_instance: str | None = "https://login.microsoftonline.com"
+    auth_tenant_id: str | None = None
+    auth_client_id: str | None = None
+    auth_client_secret: str | None = None
+
+    # database settings
     db_user: str
     db_password: str
     db_host: str
     db_port: int
     db_name: str
     nachet_schema: str
+
+    # blob storage settings
     azure_storage_account_name: str | None = None
     azure_storage_account_key: str | None = None
     azure_storage_default_endpoint_protocol: str | None = None
     azure_storage_endpoint_suffix: str | None = None
-    openai_api_deployment: str = Field(alias="azure_openai_deployment")
-    openai_api_endpoint: str = Field(alias="azure_openai_endpoint")
-    openai_api_key: str = Field(alias="azure_openai_key")
-    phoenix_endpoint: str | None = None
+
+    # api settings
+    project_name: str = "Nachet API"
     swagger_path: str = "/docs"
-    # upload_folder: str = "uploads"
+    swagger_ui_client_id: str | None = None
     allowed_origins: list[str]
-    otel_exporter_otlp_endpoint: str = Field(alias="otel_exporter_otlp_endpoint")
     testing: bool = False
+    debug: bool = False
 
     @computed_field
     @property
@@ -175,30 +162,11 @@ def create_app(settings: Settings, router: APIRouter, lifespan=None):
 
     app.engine = create_engine(**settings.db_conn_info)
 
-    ocr = OCR(api_endpoint=settings.api_endpoint, api_key=settings.api_key)
-    app.ocr = ocr
-
-    gpt = GPT(
-        api_endpoint=settings.openai_api_endpoint,
-        api_key=settings.openai_api_key,
-        deployment_id=settings.openai_api_deployment,
-        # phoenix_endpoint=settings.phoenix_endpoint,
-    )
-    app.gpt = gpt
-
     app.include_router(router)
 
-    minio_client = Minio(
-        settings.minio_endpoint,
-        access_key=settings.minio_access_key,
-        secret_key=settings.minio_secret_key,
-        secure=settings.minio_secure,
-    )
+    # storage = FertiscanStorage(sm, settings.minio_app_bucket)
 
-    sm = MinIOStorageManager(minio_client)
-    storage = FertiscanStorage(sm, settings.minio_app_bucket)
-
-    app.storage = storage
+    # app.storage = storage
 
     @app.exception_handler(Exception)
     async def global_exception_handler(_: Request, e: Exception):
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,27 +1,28 @@
 import hypercorn
-from fastapi import FastAPI
+# from fastapi import FastAPI
 
 from api.routes import router as api_router
-from core import config
+from api.config import create_app, lifespan, Settings
+# from core import config
 
 
-def get_application() -> FastAPI:
-    application = FastAPI(
-        title=config.PROJECT_NAME,
-        debug=config.DEBUG,
-        version=config.VERSION,
-        swagger_ui_oauth2_redirect_url='/oauth2-redirect',
-        swagger_ui_init_oauth={
-            "usePkceWithAuthorizationCodeGrant": True,
-            "clientId": config.SWAGGER_UI_CLIENT_ID,
-            "scopes": [f'api://{config.API_CLIENT_ID}/access_as_user']
-        }
-    )
-    application.include_router(api_router)
-    return application
+# def get_application() -> FastAPI:
+#     application = FastAPI(
+#         title=config.PROJECT_NAME,
+#         debug=config.DEBUG,
+#         version=config.VERSION,
+#         swagger_ui_oauth2_redirect_url='/oauth2-redirect',
+#         swagger_ui_init_oauth={
+#             "usePkceWithAuthorizationCodeGrant": True,
+#             "clientId": config.SWAGGER_UI_CLIENT_ID,
+#             "scopes": [f'api://{config.API_CLIENT_ID}/access_as_user']
+#         }
+#     )
+#     application.include_router(api_router)
+#     return application
 
 
-app = get_application()
+app = create_app(Settings(), api_router, lifespan=lifespan)
 
 
 if __name__ == "__main__":
