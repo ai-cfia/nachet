f34f609626960eaef6f7798f4421daabd4462bcb
Joseffus Santos <joseffus.santos@inspection.gc.ca>
2025-09-10 04:23:27 +0000

    fix test

--- backend/tests/test_health_request.py
+++ backend/tests/test_health_request.py
@@ -1,8 +1,10 @@
 import pytest
-from app import app
+from fastapi.testclient import TestClient
+from app.main import app
 
 @pytest.mark.asyncio
 async def test_health():
-    test = app.test_client()
+    test = TestClient(app)
     response = await test.get('/health')
     assert response.status_code == 200
+    assert response.json() == {"status": "ok"}
