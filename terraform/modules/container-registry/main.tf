9a5ae806b989152932e4aa5efa184afd4dc0ba6d
ThomasCardin <tomcardin@outlook.com>
2025-09-15 09:56:40 -0400

    test ACR with allow all ip

--- terraform/modules/container-registry/main.tf
+++ terraform/modules/container-registry/main.tf
@@ -20,7 +20,7 @@ resource "azurerm_container_registry" "main" {
   public_network_access_enabled = false
 
   network_rule_set {
-    default_action = "Deny"
+    default_action = "Allow"
     
     ip_rule {
       action   = "Allow"
