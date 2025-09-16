af05830a35386a1950404b50cdfa5dd66cfc85e5
ThomasCardin <tomcardin@outlook.com>
2025-09-16 14:25:16 +0000

    test terraform init within azurecli window

--- terraform/dev/providers.tf
+++ terraform/dev/providers.tf
@@ -17,7 +17,6 @@ terraform {
     storage_account_name = "tfstatenachet"
     container_name       = "tfstate"
     key                  = "tf/nachet-dev.tfstate"
-    use_azuread_auth = true
   }
 }
 
