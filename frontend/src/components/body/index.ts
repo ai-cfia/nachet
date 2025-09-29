1794180b368423ad573c0456edeff614a00cd200
GitHub Action <actions@github.com>
2025-09-29 23:01:08 +0000

    Release nachet-backend-v1.14.0

--- frontend/src/components/body/index.ts
+++
@@ -1,5 +0,0 @@
-import BatchUploadPopup from "./batch_upload_popup/BatchUploadPopup";
-import CreateDirectoryPopup from "./create_directory_popup/CreateDirectoryPopup";
-import DeleteDirectoryPopup from "./del_directory_popup/DeleteDirectoryPopup";
-
-export { BatchUploadPopup, CreateDirectoryPopup, DeleteDirectoryPopup };
--- frontend/src/hooks/index.tsx
+++
@@ -1,6 +0,0 @@
-import useDecoderTiff from "./useDecoderTiff";
-import useBackendUrl from "./useBackendUrl";
-import { useAuth } from "./useAuth";
-import { useSpeciesData } from "./useSpeciesData";
-
-export { useDecoderTiff, useBackendUrl, useAuth, useSpeciesData };
