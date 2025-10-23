74196238bed05cfbf9c8c1779aa529779513f3a2
GitHub Action <actions@github.com>
2025-10-23 04:07:37 +0000

    Release nachet-backend-v1.37.0

--- frontend/src/hooks/index.tsx
+++
@@ -1,6 +0,0 @@
-import useDecoderTiff from "./useDecoderTiff";
-import useBackendUrl from "./useBackendUrl";
-import { useSpeciesData } from "./useSpeciesData";
-import { useDeviceData } from "./useDeviceData";
-
-export { useDecoderTiff, useBackendUrl, useSpeciesData, useDeviceData };
