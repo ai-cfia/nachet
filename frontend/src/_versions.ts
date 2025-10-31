19726bca4618a6f72348872c61a0dca829b11ae3
Joseffus Santos <joseffus.santos@inspection.gc.ca>
2025-10-31 08:42:58 +0000

    Bump version to 1.0.0 and update version date to 2025-10-31T08:42:31.838Z

--- frontend/src/_versions.ts
+++ frontend/src/_versions.ts
@@ -9,8 +9,8 @@ export interface TsAppVersion {
     gitTag?: string;
 };
 export const versions: TsAppVersion = {
-    version: '0.18.0',
+    version: '1.0.0',
     name: 'nachet-frontend',
-    versionDate: '2025-10-31T02:02:19.791Z',
+    versionDate: '2025-10-31T08:42:31.838Z',
 };
 export default versions;
