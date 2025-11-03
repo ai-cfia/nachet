94cb6679aafaae5436d650e929e4b03743f5dc63
Joseffus Santos <joseffus.santos@inspection.gc.ca>
2025-11-03 03:47:53 +0000

    feat: add PopupActionButtons and SampleMetadataFields components; refactor DeviceSelectionFields layout

---
+++ frontend/src/components/common/index.ts
@@ -0,0 +1,11 @@
+import { PopupActionButtons } from "./PopupActionButtons";
+import { DeviceSelectionFields } from "./DeviceSelectionFields";
+import { SampleMetadataFields } from "./SampleMetadataFields";
+import { ToastNotification } from "./ToastNotification";
+
+export {
+  PopupActionButtons,
+  DeviceSelectionFields,
+  SampleMetadataFields,
+  ToastNotification,
+};
