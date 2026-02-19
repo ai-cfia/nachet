628c4638b31bc8f1bdb70150ec6dceff7c74752e
Joseffus Santos <joseffus.santos@inspection.gc.ca>
2026-02-19 21:53:46 +0000

    phase 1 dependencies path aliases

--- nachet-mini/vite.config.ts
+++ nachet-mini/vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@common": path.resolve(__dirname, "src/common"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@stores": path.resolve(__dirname, "src/stores"),
+      "@inference": path.resolve(__dirname, "src/inference"),
+    },
+  },
+});
