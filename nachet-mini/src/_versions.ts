3fea6bc52d80dc69372885addf43d48328d3d323
Joseffus Santos <joseffus.santos@inspection.gc.ca>
2026-02-19 22:05:25 +0000

    Phase 2 import utility functions

---
+++ nachet-mini/src/_versions.ts
@@ -0,0 +1,16 @@
+export interface TsAppVersion {
+    version: string;
+    name: string;
+    description?: string;
+    versionLong?: string;
+    versionDate: string;
+    gitCommitHash?: string;
+    gitCommitDate?: string;
+    gitTag?: string;
+};
+export const versions: TsAppVersion = {
+    version: '0.0.1',
+    name: 'nachet-mini',
+    versionDate: '2026-02-19T22:04:05.278Z',
+};
+export default versions;
