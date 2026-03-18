4c9b84ab97e7aeef10474a61cf596bb4011f25ad
Joseffus Santos <joseffus.santos@inspection.gc.ca>
2026-03-18 02:11:59 +0000

    version bump

--- nachet-mini/src/_versions.ts
+++ nachet-mini/src/_versions.ts
@@ -1,16 +1,16 @@
 export interface TsAppVersion {
-  version: string;
-  name: string;
-  description?: string;
-  versionLong?: string;
-  versionDate: string;
-  gitCommitHash?: string;
-  gitCommitDate?: string;
-  gitTag?: string;
-}
+    version: string;
+    name: string;
+    description?: string;
+    versionLong?: string;
+    versionDate: string;
+    gitCommitHash?: string;
+    gitCommitDate?: string;
+    gitTag?: string;
+};
 export const versions: TsAppVersion = {
-  version: "0.1.1",
-  name: "nachet-mini",
-  versionDate: "2026-03-18T01:24:09.866Z",
+    version: '0.2.0',
+    name: 'nachet-mini',
+    versionDate: '2026-03-18T02:11:47.371Z',
 };
 export default versions;
