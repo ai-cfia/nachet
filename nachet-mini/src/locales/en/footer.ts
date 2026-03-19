2952f4d047ddafc9d93b3c15ec1ed355359c0bd7
GitHub Action <actions@github.com>
2026-03-19 15:31:43 +0000

    Release frontend nachet-frontend-v2.9.4

--- nachet-mini/src/locales/en/footer.ts
+++
@@ -1,8 +0,0 @@
-const footer = {
-  developedBy: "Developed by AI Lab",
-  version: "Version: {{version}}",
-  reportIssue: "Report an Issue",
-  canadaLogoAlt: "Canada Logo",
-} as const;
-
-export default footer;
