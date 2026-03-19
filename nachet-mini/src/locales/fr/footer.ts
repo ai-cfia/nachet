0874ed52c8d2dd02b7c87aa67545a32fee221183
GitHub Action <actions@github.com>
2026-03-19 15:01:16 +0000

    Release nachet-backend-v2.9.4

--- nachet-mini/src/locales/fr/footer.ts
+++
@@ -1,8 +0,0 @@
-const footer = {
-  developedBy: "D\u00e9velopp\u00e9 par le laboratoire d\u2019IA",
-  version: "Version\u00a0: {{version}}",
-  reportIssue: "Signaler un problème",
-  canadaLogoAlt: "Logo du Canada",
-} as const;
-
-export default footer;
