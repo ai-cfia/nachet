d87801c774109a41b0c1f25db04b1c929bbdeff2
GitHub Action <actions@github.com>
2026-05-05 20:56:27 +0000

    Release nachet-mini nachet-mini-v0.10.2

--- terraform/modules/pgadmin/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "pgadmin_url" {
-  description = "Internal URL for PgAdmin (accessible from VNet only)"
-  value       = "https://${azurerm_container_app.pgadmin.latest_revision_fqdn}"
-}
-
-output "pgadmin_container_app_name" {
-  description = "PgAdmin Container App name"
-  value       = azurerm_container_app.pgadmin.name
-}
