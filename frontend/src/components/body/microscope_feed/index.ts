cef76bcbc83536a798f31caba060a6959071e067
GitHub Action <actions@github.com>
2026-07-13 20:28:22 +0000

    Release nachet-mini nachet-mini-v0.10.13

--- frontend/src/components/body/microscope_feed/index.ts
+++
@@ -1,9 +0,0 @@
-import MicroscopeFeed from "./MicroscopeFeed";
-
-export { MicroscopeFeedControlsView } from "./MicroscopeFeedControlsView";
-export type { MicroscopeFeedControlsViewProps } from "./MicroscopeFeedControlsView";
-
-export { MicroscopeFeedWorkspaceView } from "./MicroscopeFeedWorkspaceView";
-export type { MicroscopeFeedWorkspaceViewProps } from "./MicroscopeFeedWorkspaceView";
-
-export default MicroscopeFeed;
