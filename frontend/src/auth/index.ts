d9a89fd32ae3bf447d12e49dcc65bc73aaa1b888
GitHub Action <actions@github.com>
2026-07-13 19:00:49 +0000

    Release nachet-backend-v2.9.20

--- frontend/src/auth/index.ts
+++
@@ -1,8 +0,0 @@
-export { NachetAuthProvider } from "./NachetAuthProvider";
-export { useNachetAuth } from "./NachetAuthContext";
-export type {
-  AuthProviderKind,
-  NachetAuthAccount,
-  NachetAuthContextValue,
-  NachetAuthTokenOptions,
-} from "./NachetAuthContext";
