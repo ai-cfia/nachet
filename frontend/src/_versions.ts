cdda2495a2760e6e6ca8b58efaa417b0f80b7ea7
youssefjedidi <Youssef.Jedidi@inspection.gc.ca>
2026-07-27 15:20:44 -0400

    Issue #877: Bump frontend and Nachet Mini versions

--- frontend/src/_versions.ts
+++ frontend/src/_versions.ts
@@ -1,16 +1,17 @@
+// generated file, do not edit
 export interface TsAppVersion {
-    version: string;
-    name: string;
-    description?: string;
-    versionLong?: string;
-    versionDate: string;
-    gitCommitHash?: string;
-    gitCommitDate?: string;
-    gitTag?: string;
-};
+  version: string;
+  name: string;
+  description?: string;
+  versionLong?: string;
+  versionDate: string;
+  gitCommitHash?: string;
+  gitCommitDate?: string;
+  gitTag?: string;
+}
 export const versions: TsAppVersion = {
-    version: '3.0.3',
-    name: 'nachet-frontend',
-    versionDate: '2026-07-14T19:30:00.000Z',
+  version: "3.0.4",
+  name: "nachet-frontend",
+  versionDate: "2026-07-27T19:19:46.657Z",
 };
 export default versions;
